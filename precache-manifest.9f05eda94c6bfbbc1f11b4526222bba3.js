11adbe1d09591eb959b8d47c043be7f2b7387e6e
ishaan[Tan] <tan.n@shopstack.asia>
2019-02-28 14:14:12 +0700

    Updates

---
+++ precache-manifest.9f05eda94c6bfbbc1f11b4526222bba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lession-44/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9614a23f009f59a064b9",
+    "url": "/lession-44/static/js/runtime~main.9614a23f.js"
+  },
+  {
+    "revision": "408da3bc66a70d9453ba",
+    "url": "/lession-44/static/js/main.408da3bc.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/lession-44/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "408da3bc66a70d9453ba",
+    "url": "/lession-44/static/css/main.cdb5e423.chunk.css"
+  },
+  {
+    "revision": "08201aeb31a2e70dc0465da7cfb8a15f",
+    "url": "/lession-44/index.html"
+  }
+];
